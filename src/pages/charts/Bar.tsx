f535ba9949f606829830c33ce56a2a2618aa4158
arhamsc <shaik.arham@gmail.com>
2022-08-17 10:33:16 +0530

    Added Column Chart and Dougnut Chart

--- src/pages/charts/Bar.tsx
+++ src/pages/charts/Bar.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import {
+    AxisModel,
+    Category,
+    ChartComponent,
+    Inject,
+    DataLabel,
+    BarSeries,
+    Tooltip,
+    Legend,
+    SeriesCollectionDirective,
+    SeriesDirective,
+    ColumnSeries,
+} from "@syncfusion/ej2-react-charts";
+import React from "react";
+import PageBox from "../../components/Layout/PageBox";
+import Header from "../../components/Layout/UIComponents/Header";
+import {
+    barCustomSeries,
+    barPrimaryXAxis,
+    barPrimaryYAxis,
+} from "../../data/dummyData/barData";
 
 const Bar = () => {
-  return (
-    <div>Bar</div>
-  )
-}
+    return (
+        <PageBox>
+            <Header title="Bar" category="Charts" />
+            <ChartComponent
+                id="barchart"
+                primaryXAxis={barPrimaryXAxis as AxisModel}
+                primaryYAxis={barPrimaryYAxis as AxisModel}
+                title="Olympic Medals"
+                tooltip={{
+                    enable: true,
+                    enableMarker: true,
+                }}>
+                <SeriesCollectionDirective>
+                    {barCustomSeries.map((item, index) => (
+                        <SeriesDirective key={index} {...item} />
+                    ))}
+                </SeriesCollectionDirective>
+                <Inject
+                    services={[ColumnSeries, Tooltip, Legend, DataLabel, Category]}
+                />
+            </ChartComponent>
+        </PageBox>
+    );
+};
 
-export default Bar
+export default Bar;
--- src/pages/charts/Pie.tsx
+++ src/pages/charts/Pie.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import {
+    AccumulationChartComponent,
+    AccumulationDataLabel,
+    AccumulationLegend,
+    AccumulationSeriesCollectionDirective,
+    AccumulationSeriesDirective,
+    AccumulationTooltip,
+    Inject,
+    PieSeries,
+} from "@syncfusion/ej2-react-charts";
+import PageBox from "../../components/Layout/PageBox";
+import Header from "../../components/Layout/UIComponents/Header";
+import { pieChartData } from "../../data/dummyData/pieData";
 
 const Pie = () => {
-  return (
-    <div>Pie</div>
-  )
-}
+    return (
+        <PageBox>
+            <Header title="Pie" category="Chart" />
+            <AccumulationChartComponent
+                id="yearly_sales_donut"
+                tooltip={{
+                    enable: true,
+                    enableMarker: true,
+                    format: "${point.x} : data ${point.y}",
+                }}>
+                <Inject
+                    services={[
+                        AccumulationDataLabel,
+                        PieSeries,
+                        AccumulationLegend,
+                        AccumulationTooltip,
+                    ]}
+                />
+                <AccumulationSeriesCollectionDirective>
+                    <AccumulationSeriesDirective
+                        dataSource={pieChartData}
+                        xName="x"
+                        yName="y"
+                        innerRadius="40%"
+                        height="100%"
+                        width="100%"
+                        enableTooltip={true}
+                        dataLabel={{
+                            visible: true,
+                            name: "text",
+                            position: "Inside",
+                            font: {
+                                fontWeight: "bold",
+                            },
+                        }}
+                    />
+                </AccumulationSeriesCollectionDirective>
+            </AccumulationChartComponent>
+        </PageBox>
+    );
+};
 
-export default Pie
+export default Pie;
